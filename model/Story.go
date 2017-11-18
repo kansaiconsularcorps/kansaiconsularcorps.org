8db5fac4368fec141030df937251ea168850ab8b
Eduard Urbach <e.urbach@gmail.com>
2017-11-18 15:08:55 +0100

    Story overview

---
+++ model/Story.go
@@ -0,0 +1,12 @@
+package model
+
+// Story ...
+type Story struct {
+	ID        string
+	Title     string
+	Summary   string
+	Text      string
+	Image     string
+	Created   string
+	CreatedBy string
+}
--- pages/home/home.go
+++ pages/home/home.go
@@ -1,11 +1,23 @@
 package home
 
 import (
+	"strconv"
+
 	"github.com/aerogo/aero"
 	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/components"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/model"
 )
 
 // Get ...
 func Get(ctx *aero.Context) string {
-	return ctx.HTML(components.Home())
+	stories := []*model.Story{}
+
+	for i := 1; i <= 10; i++ {
+		stories = append(stories, &model.Story{
+			Title:   "Story " + strconv.Itoa(i),
+			Summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor est, iaculis nec interdum eu, gravida et tortor. Morbi eleifend est sit amet dolor tristique vestibulum. Quisque leo urna, placerat vel dapibus in, elementum vitae lacus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In placerat lorem sed nisi posuere varius.",
+			Image:   "http://d1dqs20vqfxmh2.cloudfront.net/wp-content/uploads/2017/06/DSC_0333_low-res-306x192.jpg",
+		})
+	}
+	return ctx.HTML(components.Home(stories))
 }
