137f6dffa15b1f1feeaf9346dde105f6043f0df9
Eduard Urbach <e.urbach@gmail.com>
2017-11-18 14:38:56 +0100

    Implemented basic layout

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"github.com/aerogo/aero"
 	"github.com/aerogo/layout"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/components/css"
 	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/components/js"
 	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/layout"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/pages/about"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/pages/contact"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/pages/gallery"
 	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/pages/home"
 )
 
@@ -14,10 +18,20 @@ func main() {
 }
 
 func configure(app *aero.Application) *aero.Application {
+	// Layout
 	l := layout.New(app)
 	l.Render = fullpage.Render
-	l.Page("/", home.Get)
 
+	// Routing
+	l.Page("/", home.Get)
+	l.Page("/about", about.Get)
+	l.Page("/gallery", gallery.Get)
+	l.Page("/contact", contact.Get)
+
+	// Styles
+	app.SetStyle(css.Bundle())
+
+	// Scripts
 	scripts := js.Bundle()
 
 	app.Get("/scripts", func(ctx *aero.Context) string {
---
+++ pages/contact/contact.go
@@ -0,0 +1,11 @@
+package contact
+
+import (
+	"github.com/aerogo/aero"
+	"github.com/kansaiconsularcorps/kansaiconsularcorps.org/components"
+)
+
+// Get ...
+func Get(ctx *aero.Context) string {
+	return ctx.HTML(components.Contact())
+}
